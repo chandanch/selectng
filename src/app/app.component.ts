ebc7feb704d8eed99c25f38a00c6d490fe26887b
chandanch <chandan.cec107@gmail.com>
2017-10-17 23:41:35 +0530

    dependent selector with default value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ export class AppComponent implements OnInit {
     new Team(2, 'Manchester'),
     new Team(3, 'Liverpool'),
   ];
+  playersList: Player[] = [];
   public activeTeam;
+  activePlayer;
 
   ngOnInit() {
     this.activeTeam = this.teamList[1];
@@ -20,10 +22,25 @@ export class AppComponent implements OnInit {
 
   getTeam(team) {
     console.log(team);
+    this.loadPlayers();
+  }
+
+  loadPlayers() {
+    this.playersList.push(
+      new Player(1, 'Jorge'),
+      new Player(2, 'Simopp'),
+      new Player(3, 'Fioas'),
+      new Player(4, 'Goerge')
+    );
+    this.activePlayer = this.playersList[1];
   }
 
   sendTeam(team) {
     console.log(`Team ${team.name} sent`);
+  }
+
+  getPlayer(player) {
+    console.log(player);
   }
 
   compareFn(c1: Team, c2: Team): boolean {
@@ -39,6 +56,16 @@ export class Team {
   constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
+  }
+}
+
+export  class Player {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.name = name;
+    this.id = id;
   }
 }
 
